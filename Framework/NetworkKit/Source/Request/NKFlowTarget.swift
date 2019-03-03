354e3fbeba849f00dba5e60d3da8af3344d8088c
John Lima <thejohnlima@icloud.com>
2019-03-03 14:23:06 -0300

    Create framework to improve network layer

---
+++ Framework/NetworkKit/Source/Request/NKFlowTarget.swift
@@ -0,0 +1,77 @@
+//  MIT License
+//
+//  Copyright (c) 2019 John Lima
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import Foundation
+
+public enum NKHTTPMethods: String {
+  case get = "GET"
+  case post = "POST"
+  case update = "PUT"
+  case delete = "DELETE"
+  case head = "HEAD"
+  case patch = "PATCH"
+}
+
+public enum NKParameterEncoding {
+  case queryString
+  case httpBody
+}
+
+public enum Task {
+  /// A request with no additional data.
+  case requestPlain
+
+  /// A requests body set with data.
+  case requestData(Data)
+
+  /// A request body set with `Encodable` type
+  case requestJSONEncodable(Encodable)
+
+  /// A requests body set with encoded parameters combined with url parameters.
+  case requestCompositeParameters(bodyParameters: NKCommon.JSON, urlParameters: NKCommon.JSON)
+
+  /// A requests body set with encoded parameters.
+  case requestParameters(_ parameters: NKCommon.JSON, encoding: NKParameterEncoding)
+
+  /// A "multipart/form-data" upload task.
+  case uploadMultipart([NKFlowMultipartFormData])
+}
+
+public protocol NKFlowTarget {
+  /// The Domain
+  var baseURL: URL { get }
+
+  /// The path to be appended to `baseURL` to form the full `URL`.
+  var path: String { get }
+
+  /// The HTTP method used in the request.
+  var method: NKHTTPMethods { get }
+
+  /// The headers to be used in the request.
+  var headers: NKCommon.HTTPHeader? { get }
+
+  /// The task to be used in the request.
+  var task: Task { get }
+
+  /// The app environment: `develop` or `production`
+  var environment: NKEnvironment { get }
+}
---
+++ README.md
@@ -0,0 +1,57 @@
+# NetworkKit
+
+[![GitHub release](https://img.shields.io/github/release/limadeveloper/NetworkKit.svg)](https://github.com/limadeveloper/NetworkKit/releases)
+[![Build Status](https://travis-ci.com/limadeveloper/NetworkKit.svg?branch=master)](https://travis-ci.com/limadeveloper/NetworkKit)
+[![CocoaPods](https://img.shields.io/badge/Cocoa%20Pods-✓-4BC51D.svg?style=flat)](https://cocoapods.org/pods/NetworkKit)
+
+**NetworkKit** is the easiest way to create your network layer in Swift.
+
+## Requirements
+
+- iOS 9.3+
+- Swift 4.0+
+
+## Installation
+
+### CocoaPods
+
+**NetworkKit** is available through [CocoaPods](https://cocoapods.org/pods/NetworkKit). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'NetworkKit', '~> 1.0'
+```
+
+and run `pod install`
+
+### Manual
+
+Just copy source folder to your project.
+
+```script
+Framework > NetworkKit > Source
+```
+
+## How to use
+
+Import library in your swift file:
+
+```Swift
+import NetworkKit
+```
+
+Example:
+
+```Swift
+
+```
+
+## Communication
+
+- If you found a bug, open an issue.
+- If you have a feature request, open an issue.
+- If you want to contribute, submit a pull request. 👨🏻‍💻
+
+## License
+
+**NetworkKit** is under MIT license. See the [LICENSE](https://raw.githubusercontent.com/limadeveloper/NetworkKit/master/LICENSE) file for more info.
